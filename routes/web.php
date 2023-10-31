9691d33f7529b811dfaf4a5a4cc83dd93e6c1b14
Matias Rossini <rossinimatias93@gmail.com>
2023-10-31 19:55:07 -0300

    re-estructuracion

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DataFeedController;
-use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,20 +8,11 @@ use App\Http\Controllers\DashboardController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::redirect('/', 'login');
-
-Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-
-    // Route for the getting the data feed
-    Route::get('/json-data-feed', [DataFeedController::class, 'getDataFeed'])->name('json_data_feed');
-
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::fallback(function() {
-        return view('pages/utility/404');
-    });    
+Route::get('/', function () {
+    return view('login');
 });
